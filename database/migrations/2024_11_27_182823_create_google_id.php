4e855a6f486c26622649c6582cc517ce43da1f16
JoaquitoGuevara <joaquitoguevara@gmail.com>
2024-11-27 15:29:25 -0300

    Add google_id

--- database/migrations/2024_11_27_182823_create_google_id.php
+++ database/migrations/2024_11_27_182823_create_google_id.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('apple_id')->nullable()->unique();
+            $table->string('google_id')->nullable()->unique();
         });
     }
 
@@ -21,8 +21,5 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
     }
 };
