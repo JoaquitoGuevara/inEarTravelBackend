e9e244a4324169bb89a507c6d3eb34eb0c4dd89c
JoaquitoGuevara <joaquitoguevara@gmail.com>
2025-01-18 03:06:57 -0300

    added sampleAudioUrl

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    protected $fillable = ['name', 'price', 'audioFile', 'photo', 'description', 'iapProductId'];
+    protected $fillable = ['name', 'price', 'audioFile', 'photo', 'description', 'iapProductId', 'sampleAudioFile'];
 
     protected $hidden = ['audioFile'];
 
